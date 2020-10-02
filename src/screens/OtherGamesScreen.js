63992f8eefa62fd140661d2b4a56dc883aa02321
Saulo Joab <saulojoabps@gmail.com>
2020-10-01 23:17:21 -0300

    converting othergamescreen to functional component

--- src/screens/OtherGamesScreen.js
+++ src/screens/OtherGamesScreen.js
@@ -1,15 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import NavigatorBar from '../components/NavigatorBar'
 
-export default class OtherGamesScreen extends Component {
-  render() {
-    return (
-        <View style={styles.container}>
-            <Text>asdn asjnjd nasjnd jan daj</Text>            
-        </View>
-    );
-  }
+export default function OtherGamesScreen () {
+  return (
+      <View style={styles.container}>
+          <Text>asdn asjnjd nasjnd jan daj</Text>            
+      </View>
+  );
 }
 
 const styles = StyleSheet.create({
